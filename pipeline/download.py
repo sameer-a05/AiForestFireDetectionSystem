0e3361e4780211efa590492e403b0897b5fff302
sameer-a05 <sameer.alshamiri3@gmail.com>
2026-07-11 17:01:14 -0400

    feature/perBand-bucketing

--- pipeline/download.py
+++ pipeline/download.py
@@ -38,52 +38,61 @@ def data_pipeline():
             
         # 3. Append the full item object to that date's list
         daily_items[date_str].append(item)
-
+        
     # target certain day for mosaicking (Dynamic Date Selection)
     target_date = max(daily_items, key=lambda k: len(daily_items[k]))
     target_items = daily_items[target_date]
     print(f"Selected optimal date: {target_date} with {len(target_items)} tiles.")
 
-    rasters_to_merge = []
     
-    # loop open, prep, and store each tile
+    
+    band_bucket = { "B04" : [],"B08" : [],"B12" : [] }
+    """
+    "B04": [ spatial_array_tile_1, spatial_array_tile_2, spatial_array_tile_3 ]
+    """
+
+    # loop open, prep, and store each tile 
     for item in target_items:
-        signed_href = planetary_computer.sign(item.assets["B04"].href)
-        ds = rioxarray.open_rasterio(
-            signed_href, 
-            masked=True
-        ).squeeze()
+        for band_name in ["B04", "B08", "B12"]:
+            signed_href = planetary_computer.sign(item.assets[band_name].href)
+            ds = rioxarray.open_rasterio(
+                signed_href, 
+                masked=True
+            ).squeeze()
+
+            band_bucket[band_name].append(ds)
         
-        rasters_to_merge.append(ds)
-
-    # stitch the 3 arrays together using their internal coordinates
-    merged_ds = merge_arrays(rasters_to_merge)
-
-    # clip the newly stitched, massive dataset to your exact bounding box
-    final_clipped = merged_ds.rio.clip_box(*bbox, crs="EPSG:4326")
-
+    
     # Dynamic File Pathing setup
     script_dir = Path(__file__).parent
     output_dir = script_dir.parent / "data" / "raw" / "sentinel2"
     output_dir.mkdir(parents=True, exist_ok=True)
     
-    filename = f"scene_{target_date.replace('-', '')}_B04_merged.tif"
-    output_path = output_dir / filename
+    processed_bands = {}
+    for band, tile_list in band_bucket.items():
+        # stitch the 3 arrays together using their internal coordinates
+        merged_ds = merge_arrays(tile_list)
+        # clip the newly stitched, massive dataset to your exact bounding box
+        final_clipped = merged_ds.rio.clip_box(*bbox, crs="EPSG:4326")    
+        filename = f"scene_{target_date.replace('-', '')}_{band}_merged.tif"
+        output_path = output_dir / filename
     
-    # Export to GeoTIFF
-    final_clipped.rio.to_raster(output_path)
-    print(f"Saved GeoTIFF to: {output_path}")
+        # Export to GeoTIFF
+        final_clipped.rio.to_raster(output_path)
+        print(f"Saved GeoTIFF to: {output_path}")
+        
+        processed_bands[band] = final_clipped
 
-    return final_clipped, target_date
+    return processed_bands, target_date
 
 
-def visualize(clip, date_string):
+def visualize(clip, date_string, band_name):
     plt.figure(figsize=(8, 6))
     plt.imshow(clip.values, cmap="gray")
-    plt.title(f"Sentinel-2 Band 4 — {date_string}")
+    plt.title(f"Sentinel-2 {band_name} — {date_string}")
     plt.colorbar()
     plt.show()
 
-
-clip, optimal_date = data_pipeline()
-visualize(clip, optimal_date)
+processed_bands , optimal_date = data_pipeline()
+for band_name, spatial_array in processed_bands.items():
+    visualize(spatial_array, optimal_date, band_name)
